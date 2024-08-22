fba9c558041ddda7d15fa6ae38e6981b68a0d62e
ethanliu <liu.ethan2005@gmail.com>
2024-08-22 18:58:17 -0400

    Completed all exercises from chapter 2

---
+++ baby.hs
@@ -0,0 +1,5 @@
+doubleMe x = x + x
+doubleUs x y = doubleMe x + doubleMe y
+doubleSmallNumber x = if x > 100 then x else x * 2
+boomBangs lst = [if x < 10 then "BOOM" else "BANG" | x <- lst, odd x]
+triangles = [(a, b, c) | c <- [1..10], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2, a+b+c == 24]
